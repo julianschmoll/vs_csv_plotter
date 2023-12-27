a88a22c2a07e431f0d3873d229ec99c3614b426b
julianschmoll <github@julianschmoll.de>
2023-12-28 00:25:01 +0100

    removing version requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pandas==1.3.3',
-        'matplotlib==3.4.3',
+        'pandas',
+        'matplotlib',
     ],
 )

54a3485c018e6940da9a995351cd27a376bf82bc
julianschmoll <github@julianschmoll.de>
2023-12-28 15:54:25 +0100

    make class based

--- vs_csv_plotter/generate_plots.py
+++ vs_csv_plotter/generate_plots.py
@@ -2,14 +2,18 @@
 # Import local modules
 from scripts import file_utils, plots
 
-def main():
-    """Generate Plots"""
-    # Your main script logic goes here
-    combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
-    file_utils.prepare_plot_folder()
-    plots.plot_participation(combined_data)
-    plots.plot_age_distribution(combined_data)
-    plots.plot_ticket_data(combined_data)
+class PlotGenerator:
+    def __init__(self):
+        self.combined_data = None
+
+    def generate_plots(self):
+        """Generate Plots"""
+        self.combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
+        file_utils.prepare_plot_folder()
+        plots.plot_participation(self.combined_data)
+        plots.plot_age_distribution(self.combined_data)
+        plots.plot_ticket_data(self.combined_data)
 
 if __name__ == "__main__":
-    main()
+    plot_generator = PlotGenerator()
+    plot_generator.generate_plots()

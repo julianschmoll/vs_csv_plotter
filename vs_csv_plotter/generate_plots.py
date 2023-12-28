15f1e651f2005f8adc6fea4816497618096c5b7a
julianschmoll <github@julianschmoll.de>
2023-12-28 18:34:14 +0100

    structure

--- vs_csv_plotter/generate_plots.py
+++ vs_csv_plotter/generate_plots.py
@@ -3,16 +3,21 @@
 from scripts import file_utils, plots
 
 class PlotGenerator:
+    """Generates Plots-"""
     def __init__(self):
-        self.combined_data = None
+        self.gather_data()
+        file_utils.prepare_plot_folder()
+
+    def gather_data(self):
+        """Gather Data from CSV Folder."""
+        self.combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
 
     def generate_plots(self):
         """Generate Plots"""
-        self.combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
-        file_utils.prepare_plot_folder()
         plots.plot_participation(self.combined_data)
         plots.plot_age_distribution(self.combined_data)
         plots.plot_ticket_data(self.combined_data)
+        plots.plot_financial_impact(self.combined_data)
 
 if __name__ == "__main__":
     plot_generator = PlotGenerator()

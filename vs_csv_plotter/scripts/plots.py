f6de3835a04b41abed76bb94c0d0bd942e1238b5
julianschmoll <github@julianschmoll.de>
2023-12-29 00:02:31 +0100

    add support data plots

--- vs_csv_plotter/scripts/plots.py
+++ vs_csv_plotter/scripts/plots.py
@@ -91,10 +91,81 @@ def plot_ticket_data(csv_data):  # noqa: WPS210 As splitting up wouldn't make se
     under_26_d_ticket = under_26_d_ticket.rename(index={"Yes": "No, D-Ticket"})
     combined_under_26 = removed_no.add(under_26_d_ticket, fill_value=0)
 
+    support_with_d_ticket = over_26_data[over_26_data[row_index] == "Yes"]
+    support_with_d_ticket_counts = support_with_d_ticket[d_ticket_index].value_counts()
+
     plot.pie(
         over_26_count, "Would you buy the JugendBW-Ticket if eligible? (>26)"
     )
-    plot.pie(over_26_d_ticket_count, "Do you own the D-Ticket? (>26)")
+    plot.pie(
+        over_26_d_ticket_count, "Do you own the D-Ticket? (>26)"
+    )
     plot.pie(
         combined_under_26, "Do you currently have the JugendBW-Ticket? (≤26)"
+    )
+    plot.pie(
+        support_with_d_ticket_counts, "Wanting JugendBW-Ticket, having D-Ticket (>26)"
+    )
+
+
+def plot_support_data(csv_data):
+    """Plot general support for Ticket.
+    
+    1. Everyone
+    2. below 26
+    3. above 26
+
+    Parameters:
+        csv_data (pd.DataFrame): DataFrame containing survey data.
+
+    """
+    row_index = (
+        "Würdest du ein vollsolidarisches Deutschlandticket unterstützen?"
+    )
+
+    wealth_index = (
+        "Wie stark würde dich das vollsolidarische bundesweite Semesterticket "
+        +"finanziell treffen? (Skala 1 (kein/kleines Problem) - 10 (nicht finanzierbar))"
+    )
+    row_index_bw_ticket = (
+        "Beziehst du aktuell das Jugendticket BW / "
+        + "Würdest du das Jugendticket BW beziehen wenn du berechtigt wärst?"
+    )
+    d_ticket_index = "Beziehst du aktuell das Deutschlandticket (49 € Ticket)?"
+
+    general_count = csv_data[row_index].value_counts()
+
+    over_26_data = csv_data[csv_data["Altersklasse"] == "> 26"]
+    over_26_count = over_26_data[row_index].value_counts()
+
+    under_26_data = csv_data[csv_data["Altersklasse"] == "≤ 26"]
+    under_26_count = under_26_data[row_index].value_counts()
+
+    wealthy_data = csv_data[csv_data[wealth_index] < 4]
+    wealthy_support_count = wealthy_data[row_index].value_counts()
+
+    not_wealthy_data = csv_data[csv_data[wealth_index] > 7]
+    not_wealthy_support_count = not_wealthy_data[row_index].value_counts()
+    
+    not_having_or_wanting = csv_data[csv_data[row_index_bw_ticket] == "No"]
+    not_having_or_wanting = not_having_or_wanting[not_having_or_wanting[d_ticket_index] == "No"]
+    not_having_or_wanting_count = not_having_or_wanting[row_index].value_counts()
+    
+    plot.pie(
+        over_26_count, "Would you support a full solidarity ticket for Germany? (> 26)"
+    )
+    plot.pie(
+        general_count, "Would you support a full solidarity ticket for Germany?"
+    )
+    plot.pie(
+        under_26_count, "Would you support a full solidarity ticket for Germany? (≤26)"
+    )
+    plot.pie(
+        wealthy_support_count, "Support by not financially affected (<4)"
+    )
+    plot.pie(
+        not_wealthy_support_count, "Support by financially affected (>7)"
+    )
+    plot.pie(
+        not_having_or_wanting_count, "Currently not interested in ticket but would support solidaty Ticket"
     )

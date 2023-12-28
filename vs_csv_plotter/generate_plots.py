a7cef9999fde177571294dd64dfb69c1d808b4bb
julianschmoll <github@julianschmoll.de>
2023-12-28 12:07:32 +0100

    replace imports

--- vs_csv_plotter/generate_plots.py
+++ vs_csv_plotter/generate_plots.py
@@ -1,10 +1,12 @@
 # Import local modules
 from scripts import plots
-from scripts import file_utils
+from scripts.file_utils import replace_ger_eng
+from scripts.file_utils import concat_from_folder
+from scripts.file_utils import prepare_plot_folder
 
 
-combined_data = file_utils.concat_from_folder()
-file_utils.prepare_plot_folder()
+combined_data = replace_ger_eng(concat_from_folder())
+prepare_plot_folder()
 plots.plot_participation(combined_data)
 plots.plot_age_distribution(combined_data)
 plots.plot_ticket_data(combined_data)

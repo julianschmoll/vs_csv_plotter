08a2cdf889cb1b1189b5db4829fc80c94c1f276c
Julian Schmoll <github@julianschmoll.de>
2023-12-28 15:26:38 +0100

    make generate_plots.sh python3 to work on linux

--- generate_plots.sh
+++ generate_plots.sh
@@ -4,9 +4,9 @@
 cd "$(dirname "$0")"
 
 echo "Installing dependencies"
-python -m pip install .
+python3 -m pip install .
 
 echo "Generating Plots..."
-python vs_csv_plotter/generate_plots.py
+python3 vs_csv_plotter/generate_plots.py
 
 echo "Successfully generated plots."

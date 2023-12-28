6c2d785aed3d3e7123db867811aa13537a7b10e8
julianschmoll <github@julianschmoll.de>
2023-12-28 15:08:32 +0100

    make generate_plots.py main function

--- vs_csv_plotter/generate_plots.py
+++ vs_csv_plotter/generate_plots.py
@@ -1,9 +1,15 @@
-"""Main generation script."""
+"""Generate Plots."""
 # Import local modules
 from scripts import file_utils, plots
 
-combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
-file_utils.prepare_plot_folder()
-plots.plot_participation(combined_data)
-plots.plot_age_distribution(combined_data)
-plots.plot_ticket_data(combined_data)
+def main():
+    """Generate Plots"""
+    # Your main script logic goes here
+    combined_data = file_utils.replace_ger_eng(file_utils.concat_from_folder())
+    file_utils.prepare_plot_folder()
+    plots.plot_participation(combined_data)
+    plots.plot_age_distribution(combined_data)
+    plots.plot_ticket_data(combined_data)
+
+if __name__ == "__main__":
+    main()

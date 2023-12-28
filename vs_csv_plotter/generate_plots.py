c11f157cbc872faa44d221f969b71e57b9ed3738
julianschmoll <github@julianschmoll.de>
2023-12-28 02:49:37 +0100

    add participation plot

--- vs_csv_plotter/generate_plots.py
+++ vs_csv_plotter/generate_plots.py
@@ -5,4 +5,5 @@ from scripts import file_utils
 
 combined_data = file_utils.concat_from_folder()
 file_utils.prepare_plot_folder()
+plots.plot_participation(combined_data)
 plots.plot_age_distribution(combined_data)
--- vs_csv_plotter/scripts/plots.py
+++ vs_csv_plotter/scripts/plots.py
@@ -1,8 +1,19 @@
 # Import local modules
+import constants
 from scripts import plot_by_diagram_type as plot
 
+# Import third party modiles
+import pandas as pd
+
+
+def plot_participation(csv_data):
+    num_participants = len(csv_data)-1
+    values = [constants.STUDENTS - num_participants, num_participants]
+    labels = ["Non-Participants", "Participants"]
+    part_data = pd.Series(values, index=labels)
+    plot.pie(part_data, "Participation")
+    
     
 def plot_age_distribution(csv_data):
     age_counts = csv_data["Altersklasse"].value_counts()
-    age_percent = age_counts / age_counts.sum() * 100
-    plot.pie(age_percent, "Age Distribution")
+    plot.pie(age_counts, "Age Distribution")

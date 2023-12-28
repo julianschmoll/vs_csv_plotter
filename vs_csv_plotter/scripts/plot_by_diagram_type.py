cc2e7deb73c91fb5f3da2ab0f278e27584d1c501
julianschmoll <github@julianschmoll.de>
2023-12-28 02:12:50 +0100

    introduce `plot_by_diagram_type`

---
+++ vs_csv_plotter/scripts/plot_by_diagram_type.py
@@ -0,0 +1,34 @@
+# import build-in modules
+import os
+
+# Import local modules
+import constants
+
+# Import third-party modules
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+
+def pie(data, title, save=constants.SAVE_PLOT, show=constants.SHOW_PLOT):
+    plt.pie(
+        data, 
+        labels=data.index, 
+        autopct="%1.1f%%", 
+        startangle=90,
+        colors=constants.CUSTOM_COLORS, 
+        textprops=constants.DESCRIPTION_FONT
+    )
+    plt.title(title, constants.HEADLINE_FONT)
+    plt.annotate(
+            str(datetime.now()),
+            xy = (0.5, -0.1),
+            xycoords='axes fraction',
+            ha='center',
+            va="center",
+            fontsize=constants.FOOTNOTE_FONT["fontsize"]
+    )
+    if save:
+        plt.savefig("{0}/{1}.svg".format(constants.PLOT_FOLDER, title.replace(" ", "_")))
+    if show:
+        plt.show()
+        
